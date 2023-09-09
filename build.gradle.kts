6cb4e972646750bd498428265a163d4c12b8d0ca
syb8200 <syb8200@naver.com>
2023-09-09 17:47:41 +0900

    [CHORE] #1 dependency 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.7.20")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.45")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.library") version "8.1.1" apply false
+    id("com.google.dagger.hilt.android") version "2.45" apply false
 }
