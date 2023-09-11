49c1d45c9a8f21181aa7aff7209c6f57e6d33277
m6z1 <sonmyungj1zz@gmail.com>
2023-09-12 01:50:55 +0900

    [UI] #16 권한 설명 UI 수정

--- app/src/main/java/com/timi/seulseul/presentation/MainActivity.kt
+++ app/src/main/java/com/timi/seulseul/presentation/MainActivity.kt
@@ -10,7 +10,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_permission)
 
 
 
