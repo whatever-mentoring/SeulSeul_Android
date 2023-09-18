4ed414f3201e41eb39a365aaa716f97a76bfa7c7
knujihyeonbae <biod0424@knu.ac.kr>
2023-09-18 15:02:40 +0900

    [FEAT] #28 network setting1

--- app/src/main/java/com/timi/seulseul/presentation/apitest/ApiTestActivity.kt
+++ app/src/main/java/com/timi/seulseul/presentation/apitest/ApiTestActivity.kt
@@ -1,9 +1,16 @@
 package com.timi.seulseul.presentation.apitest
 
+import android.os.Bundle
 import com.timi.seulseul.R
 import com.timi.seulseul.databinding.ActivityMainBinding
 import com.timi.seulseul.presentation.common.base.BaseActivity
 
 class ApiTestActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_apitest) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
 
 }
