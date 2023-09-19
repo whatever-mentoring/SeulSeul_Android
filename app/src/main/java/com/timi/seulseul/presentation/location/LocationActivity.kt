f78c3cac500d881185acc6c1a7d4d20621293020
m6z1 <sonmyungj1zz@gmail.com>
2023-09-20 00:13:47 +0900

    [UI] #7 주소 설정 뷰 작업

--- app/src/main/java/com/timi/seulseul/presentation/location/LocationActivity.kt
+++ app/src/main/java/com/timi/seulseul/presentation/location/LocationActivity.kt
@@ -5,10 +5,10 @@ import com.timi.seulseul.R
 import com.timi.seulseul.databinding.ActivityMainBinding
 import com.timi.seulseul.presentation.common.base.BaseActivity
 
-class LocationActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_location) {
+class LocationActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_location_search) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_location)
+        setContentView(R.layout.activity_location_search)
 
 
     }
