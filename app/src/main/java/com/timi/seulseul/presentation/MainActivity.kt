bcc92e047495c74975c3c543c65abcb605588c19
m6z1 <sonmyungj1zz@gmail.com>
2023-09-19 14:33:11 +0900

    [ADD] #7 locationActivity 추가

--- app/src/main/java/com/timi/seulseul/presentation/MainActivity.kt
+++ app/src/main/java/com/timi/seulseul/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.timi.seulseul.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -7,12 +8,13 @@ import com.timi.seulseul.R
 import com.timi.seulseul.databinding.ActivityMainBinding
 import com.timi.seulseul.presentation.common.base.BaseActivity
 import com.timi.seulseul.presentation.dialog.AlarmBottomSheetFragment
+import com.timi.seulseul.presentation.location.LocationActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    private lateinit var bottomSheetFragment : BottomSheetDialogFragment
+    private lateinit var bottomSheetFragment: BottomSheetDialogFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -21,15 +23,23 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         binding.view = this
 
         initBottomSheetFragment()
+
+        binding.homeTvLocationSetting.setOnClickListener {
+            val intent = Intent(this, LocationActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun initBottomSheetFragment() {
         bottomSheetFragment = AlarmBottomSheetFragment()
-        bottomSheetFragment.setStyle(DialogFragment.STYLE_NORMAL, R.style.RoundCornerBottomSheetDialogTheme)
+        bottomSheetFragment.setStyle(
+            DialogFragment.STYLE_NORMAL,
+            R.style.RoundCornerBottomSheetDialogTheme
+        )
     }
 
     fun showAlarmSetting() {
-        if(!bottomSheetFragment.isAdded) {
+        if (!bottomSheetFragment.isAdded) {
             bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
         }
     }
---
+++ app/src/main/java/com/timi/seulseul/presentation/location/LocationActivity.kt
@@ -0,0 +1,12 @@
+package com.timi.seulseul.presentation.location
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.timi.seulseul.R
+
+class LocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location)
+    }
+}
