a01dfad8f2ebfb6946a6a66c5e19159c43ca8a22
syb8200 <syb8200@naver.com>
2023-09-21 01:38:47 +0900

    [FEAT] #31 bottom sheet 내부 radio button 체크 구현 및 알림 설정 후 view 처리 (로직만)

--- app/src/main/java/com/timi/seulseul/presentation/MainActivity.kt
+++ app/src/main/java/com/timi/seulseul/presentation/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.timi.seulseul.presentation
 
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
 import androidx.fragment.app.DialogFragment
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.timi.seulseul.R
+import com.timi.seulseul.data.model.AlarmInfo
 import com.timi.seulseul.databinding.ActivityMainBinding
 import com.timi.seulseul.presentation.common.base.BaseActivity
 import com.timi.seulseul.presentation.dialog.AlarmBottomSheetFragment
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-
-    private lateinit var bottomSheetFragment : BottomSheetDialogFragment
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -20,16 +22,23 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
         binding.view = this
 
-        initBottomSheetFragment()
     }
 
-    private fun initBottomSheetFragment() {
-        bottomSheetFragment = AlarmBottomSheetFragment()
-        bottomSheetFragment.setStyle(DialogFragment.STYLE_NORMAL, R.style.RoundCornerBottomSheetDialogTheme)
-    }
-
+    // onclick (알림 추가하기)
     fun showAlarmSetting() {
-        if(!bottomSheetFragment.isAdded) {
+        val bottomSheetFragment = AlarmBottomSheetFragment()
+        bottomSheetFragment.setStyle(DialogFragment.STYLE_NORMAL, R.style.RoundCornerBottomSheetDialogTheme)
+
+        bottomSheetFragment.setButtonClickListener(object : AlarmBottomSheetFragment.OnButtonClickListener {
+            override fun onOkBtnClicked() {
+                binding.homeTvAlarmAdd.visibility = View.GONE
+                binding.homeClAlarmSetting.visibility = View.VISIBLE
+                binding.homeTvAlarm.text = "알림 수신 예정"
+                binding.homeTvDay.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_home_day_checked, 0, 0, 0)
+            }
+        })
+
+        if (!bottomSheetFragment.isAdded) {
             bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
         }
     }
--- app/src/main/java/com/timi/seulseul/presentation/dialog/AlarmBottomSheetFragment.kt
+++ app/src/main/java/com/timi/seulseul/presentation/dialog/AlarmBottomSheetFragment.kt
@@ -6,39 +6,180 @@ import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.timi.seulseul.MainApplication.Companion.prefs
+import com.timi.seulseul.R
 import com.timi.seulseul.databinding.FragmentAlarmBottomSheetDialogBinding
+import timber.log.Timber
 
 class AlarmBottomSheetFragment : BottomSheetDialogFragment() {
 
-    private var binding : FragmentAlarmBottomSheetDialogBinding? = null
+
+    private var _binding: FragmentAlarmBottomSheetDialogBinding? = null
+    private val binding get() = _binding!!
+
+    private var isChecking = true
+
+    private var alarmTime : Int = 0
+    private var alarmTerm : Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         return FragmentAlarmBottomSheetDialogBinding.inflate(inflater, container, false).apply {
-            binding = this
-            binding?.view = this@AlarmBottomSheetFragment
+            _binding = this
+            binding.view = this@AlarmBottomSheetFragment
+            binding.lifecycleOwner = this@AlarmBottomSheetFragment
+            Timber.d("onCreateView : $alarmTime / $alarmTerm")
         }.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initListener()
+
         val bottomSheet = dialog?.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
         bottomSheet?.layoutParams?.height = ViewGroup.LayoutParams.MATCH_PARENT // full screen 으로 만들기 위해 필요1
 
         val behavior = BottomSheetBehavior.from<View>(bottomSheet!!)
         behavior.state = BottomSheetBehavior.STATE_EXPANDED // full screen 으로 만들기 위해 필요2
+
+        alarmTime = prefs.getInt("alarmTime", 0)
+        alarmTerm = prefs.getInt("alarmTerm", 0)
+        Timber.d("onViewCreated : $alarmTime / $alarmTerm")
+
+        // 이전 데이터가 있다면
+        setAlarmData()
+
+        checkAlarmTime()
+        checkAlarmTerm()
+
+        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when(newState) {
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                        slideBottomSheet()
+                        Timber.d("hidden")
+                    }
+                }
+            }
+            override fun onSlide(bottomSheet: View, slideOffset: Float) { }
+        })
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        binding = null
+        Timber.d("onDestroyView : $alarmTime / $alarmTerm")
+        _binding = null
     }
 
-    fun closeAlarmSetting() {
+    private fun initListener() {
+        binding.homeClAlarmBottomSheetTvSettingOk.setOnClickListener{
+            buttonClickListener.onOkBtnClicked()
+            closeBottomSheet()
+            dismiss()
+        }
+    }
+
+    fun setAlarmData() {
+        when(alarmTime) {
+            60 -> binding.itemPreferredTimeRbBeforeOneHour.isChecked = true
+            90 -> binding.itemPreferredTimeRbBeforeOneHourHalf.isChecked = true
+            120 -> binding.itemPreferredTimeRbBeforeTwoHour.isChecked = true
+            150 -> binding.itemPreferredTimeRbBeforeTwoHourHalf.isChecked = true
+            180 -> binding.itemPreferredTimeRbBeforeThreeHour.isChecked = true
+        }
+
+        binding.homeClAlarmBottomSheetTvSettingOk.setBackgroundResource(R.drawable.bg_green_500_main_r4)
+        binding.homeClAlarmBottomSheetTvSettingOk.isEnabled = true
+
+        when(alarmTerm) {
+            10 -> binding.itemTimeTermRb10.isChecked = true
+            20 -> binding.itemTimeTermRb20.isChecked = true
+            30 -> binding.itemTimeTermRb30.isChecked = true
+            40 -> binding.itemTimeTermRb40.isChecked = true
+            50 -> binding.itemTimeTermRb50.isChecked = true
+            60 -> binding.itemTimeTermRb60.isChecked = true
+        }
+    }
+
+    // 알람 희망 시간
+    private fun checkAlarmTime() {
+        binding.homeClAlarmBottomSheetRgPreferredTime.setOnCheckedChangeListener { group, i ->
+            when(i) {
+                R.id.item_preferred_time_rb_before_one_hour -> alarmTime = 60
+                R.id.item_preferred_time_rb_before_one_hour_half -> alarmTime = 90
+                R.id.item_preferred_time_rb_before_two_hour -> alarmTime = 120
+                R.id.item_preferred_time_rb_before_two_hour_half -> alarmTime = 150
+                R.id.item_preferred_time_rb_before_three_hour -> alarmTime = 180
+            }
+
+            // 알림 희망 시간 선택 시 알림 설정 버튼 색 변경
+            if (i > -1) {
+                binding.homeClAlarmBottomSheetTvSettingOk.setBackgroundResource(R.drawable.bg_green_500_main_r4)
+                binding.homeClAlarmBottomSheetTvSettingOk.isEnabled = true
+            } else {
+                binding.homeClAlarmBottomSheetTvSettingOk.setBackgroundResource(R.drawable.bg_grey_200_r4)
+                binding.homeClAlarmBottomSheetTvSettingOk.isEnabled = false
+            }
+        }
+    }
+
+    // 알림 반복
+    private fun checkAlarmTerm() {
+        binding.homeClAlarmBottomSheetRgTerm1.setOnCheckedChangeListener { group, i ->
+            if (i != -1 && isChecking) {
+                isChecking = false
+                binding.homeClAlarmBottomSheetRgTerm2.clearCheck()
+            }
+            isChecking = true
+            when(i) {
+                R.id.item_time_term_rb_10 -> alarmTerm = 10
+                R.id.item_time_term_rb_20 -> alarmTerm = 20
+                R.id.item_time_term_rb_30 -> alarmTerm = 30
+            }
+        }
+
+        binding.homeClAlarmBottomSheetRgTerm2.setOnCheckedChangeListener { group, i ->
+            if (i != -1 && isChecking) {
+                isChecking = false
+                binding.homeClAlarmBottomSheetRgTerm1.clearCheck()
+            }
+            isChecking = true
+            when(i) {
+                R.id.item_time_term_rb_40 -> alarmTerm = 40
+                R.id.item_time_term_rb_50 -> alarmTerm = 50
+                R.id.item_time_term_rb_60 -> alarmTerm = 60
+            }
+        }
+    }
+
+    // 알림 설정 버튼 눌렀을 떄
+    fun closeBottomSheet() {
+        prefs.edit().putInt("alarmTime", alarmTime).apply()
+        prefs.edit().putInt("alarmTerm", alarmTerm).apply()
+        prefs.edit().putBoolean("alarmOn", true).apply()
+        Timber.d("closeBottomSheet : $alarmTime / $alarmTerm")
+    }
+
+    // 그냥 바텀시트 내렸을 때
+    fun slideBottomSheet() {
+        Timber.d("slideBottomSheet : $alarmTime / $alarmTerm")
+
         dismiss()
     }
+
+    interface OnButtonClickListener {
+        fun onOkBtnClicked()
+    }
+
+    // 클릭 이벤트 설정
+    fun setButtonClickListener(buttonClickListener: OnButtonClickListener) {
+        this.buttonClickListener = buttonClickListener
+    }
+
+    // 클릭 이벤트 실행
+    private lateinit var buttonClickListener: OnButtonClickListener
 }
