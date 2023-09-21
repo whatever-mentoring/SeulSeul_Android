ec519b99e2703169d988d296477e693d40cf6e49
m6z1 <sonmyungj1zz@gmail.com>
2023-09-21 17:17:00 +0900

    [UI] #38 주소 검색 API 페이지 뷰 작업

--- app/src/main/java/com/timi/seulseul/presentation/location/activity/LocationActivity.kt
+++ app/src/main/java/com/timi/seulseul/presentation/location/activity/LocationActivity.kt
@@ -2,13 +2,17 @@ package com.timi.seulseul.presentation.location.activity
 
 import android.os.Bundle
 import com.timi.seulseul.R
+import com.timi.seulseul.databinding.ActivityLocationBinding
 import com.timi.seulseul.databinding.ActivityMainBinding
 import com.timi.seulseul.presentation.common.base.BaseActivity
 
-class LocationActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_location) {
+class LocationActivity : BaseActivity<ActivityLocationBinding>(R.layout.activity_location) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location)
+
+        binding.locationSearchBar.setOnClickListener {
+        }
 
 
     }
---
+++ app/src/main/java/com/timi/seulseul/presentation/location/activity/LocationSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.timi.seulseul.presentation.location.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.timi.seulseul.R
+
+class LocationSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_search)
+    }
+}
