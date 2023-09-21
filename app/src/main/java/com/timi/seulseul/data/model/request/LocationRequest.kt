0b452c6d55d8fc1f4d6fb8f0c80c55e1a966487e
m6z1 <sonmyungj1zz@gmail.com>
2023-09-22 03:30:39 +0900

    [FEAT] #35 목적지 데이터 클래스 생성

---
+++ app/src/main/java/com/timi/seulseul/data/model/request/LocationRequest.kt
@@ -0,0 +1,9 @@
+package com.timi.seulseul.data.model.request
+
+data class LocationRequest(
+    val endX : Double,
+    val endY : Double,
+    val endNickName : String,
+    val roadNameAddress : String,
+    val jibunAddress : String
+)
---
+++ app/src/main/java/com/timi/seulseul/data/model/response/LocationResponse.kt
@@ -0,0 +1,6 @@
+package com.timi.seulseul.data.model.response
+
+data class LocationResponse(
+    // 추후 추가 예정입니다. 패키지를 위한 껍데기
+    val id : Int
+)
