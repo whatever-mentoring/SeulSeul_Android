3f6bf8a94d9c98c5a74f3b0facb77c2e1444de0e
syb8200 <syb8200@naver.com>
2023-09-26 01:03:53 +0900

    [FEAT] #71 알람 on/off 서버 통신 세팅

--- app/src/main/java/com/timi/seulseul/data/api/AlarmService.kt
+++ app/src/main/java/com/timi/seulseul/data/api/AlarmService.kt
@@ -2,12 +2,14 @@ package com.timi.seulseul.data.api
 
 import com.timi.seulseul.data.model.request.AlarmPatchRequest
 import com.timi.seulseul.data.model.request.AlarmPostRequest
+import com.timi.seulseul.data.model.response.AlarmEnabledPatchResponse
 import com.timi.seulseul.data.model.response.AlarmPatchResponse
 import com.timi.seulseul.data.model.response.AlarmPostResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface AlarmService {
     @POST("v1/alarm")
@@ -15,4 +17,7 @@ interface AlarmService {
 
     @PATCH("v1/alarm")
     suspend fun patchAlarm(@Body alarm : AlarmPatchRequest) : Response<AlarmPatchResponse>
+
+    @PATCH("v1/alarm/enabled/{id}")
+    suspend fun patchAlarmEnabled(@Path("id") id : Int) : Response<AlarmEnabledPatchResponse>
 }
--- app/src/main/java/com/timi/seulseul/data/repository/AlarmRepo.kt
+++ app/src/main/java/com/timi/seulseul/data/repository/AlarmRepo.kt
@@ -3,6 +3,7 @@ package com.timi.seulseul.data.repository
 import com.timi.seulseul.data.api.AlarmService
 import com.timi.seulseul.data.model.request.AlarmPatchRequest
 import com.timi.seulseul.data.model.request.AlarmPostRequest
+import com.timi.seulseul.data.model.response.AlarmEnabledData
 import com.timi.seulseul.data.model.response.AlarmPatchData
 import com.timi.seulseul.data.model.response.AlarmPostData
 import timber.log.Timber
@@ -54,7 +55,31 @@ class AlarmRepo @Inject constructor(
                 Result.failure(RuntimeException(errorMsg))
             }
         } catch (e: Exception) {
-            Timber.e("Exception when posting Alarm : $e")
+            Timber.e("Exception when patching Alarm : $e")
+            Result.failure(e)
+        }
+    }
+
+    suspend fun patchAlarmEnabled(id : Int) : Result<AlarmEnabledData> {
+        return try {
+            val response = alarmService.patchAlarmEnabled(id)
+
+            if (response.isSuccessful) {
+                Timber.d("Patch Alarm Enabled successful ${response.body()}")
+
+                response.body()?.data?.let {
+                    Result.success(it)
+                } ?: run {
+                    Result.failure(RuntimeException("Response body is null of data is missing"))
+                }
+            } else {
+                val errorMsg = ErrorUtils.getErrorMessage(response.code())
+
+                Timber.e(errorMsg)
+                Result.failure(RuntimeException(errorMsg))
+            }
+        } catch (e: Exception) {
+            Timber.e("Exception when patching AlarmEnabled : $e")
             Result.failure(e)
         }
     }
