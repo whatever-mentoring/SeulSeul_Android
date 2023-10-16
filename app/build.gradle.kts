e73daa59e7efb9b583d0265ef0e8826a46a78f72
syb8200 <syb8200@naver.com>
2023-10-16 21:17:45 +0900

    [CHORE] #117 버전 코드 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.timi.seulseul"
         minSdk = 26
         targetSdk = 33
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
